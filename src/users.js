eec04130bf5b802136dec8fd33d7929ae524b94c
Kari-Thrains <kat22@hi.is>
2021-02-28 15:08:59 +0000

    Update users.js

--- src/users.js
+++ src/users.js
@@ -1,31 +1,8 @@
 import bcrypt from 'bcrypt';
-import pg from 'pg';
 import dotenv from 'dotenv';
 import { query } from './db.js';
 
 dotenv.config();
-// const connectionString = process.env.DATABASE_URL;
-// const pool = new pg.Pool({ connectionString });
-
-// pool.on('error', (err) => {
-//   console.error('Unexpected error on idle client', err);
-//   process.exit(-1);
-// });
-
-// export async function query(q, values = []) {
-//   const client = await pool.connect();
-//   let result;
-
-//   try {
-//     result = await client.query(q, values);
-//   } catch (err) {
-//     console.error('Villa í query', err);
-//     throw err;
-//   } finally {
-//     client.release();
-//   }
-//   return result;
-// }
 
 export async function comparePasswords(password, hash) {
   const result = await bcrypt.compare(password, hash);
